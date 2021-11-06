e86c3f902819fa4bca61bffd33247b2ac05c69a8
acbuynak <acbuynak@gmail.com>
2021-11-06 18:27:47 -0400

    Initial RealSense Camera Support

--- atturtle_vision/scripts/atturtle_vision.py
+++ atturtle_vision/scripts/atturtle_vision.py
@@ -1,5 +1,77 @@
+#!/usr/bin/env python
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+
+## Imports
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+
+
+def runner(data):
+    """
+    Callback function for image subscriber, every frame gets scanned for board and publishes to board_center topic
+    (for robot movement) and board tile centers (for game state updates)
+    :param camera_data: Camera data input from subscriber
+    """
+    
+
+    try:
+
+        # YOUR CODE HERE!
+
+
+
+    except rospy.ROSInterruptException:
+        exit()
+    except KeyboardInterrupt:
+        exit()
+
+
+
+
+
+
+
+
 def main():
-    print('Hi from atturtle_vision.')
+    """
+    Main Runner.
+    This script should only be launched via a launch script or when the Camera Node is already open.
+    """
+
+    # Setup Node
+    rospy.init_node('vision', anonymous=False)
+    rospy.loginfo(">> Vision Processor Node Successfully Created")
+
+
+    # Setup Listeners
+    image_sub = rospy.Subscriber("/camera/color/image_raw", Image, runner)
+
+
+    # Auto-Run until launch file is shutdown
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
+
 
 
 if __name__ == '__main__':
