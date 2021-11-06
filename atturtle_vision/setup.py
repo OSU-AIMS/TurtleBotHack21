e251998d5aca117af6c3cbd71f5d0fd99e73ace5
acbuynak <acbuynak@gmail.com>
2021-11-06 15:37:18 -0400

    Fix repo to use 'scripts' folder

--- atturtle_vision/setup.py
+++ atturtle_vision/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 package_name = 'atturtle_vision'
+scripts_locn = 'scripts'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[scripts_locn],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
