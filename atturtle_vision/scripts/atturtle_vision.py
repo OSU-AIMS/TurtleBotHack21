8e862a74783313923d3d2b28249f3db49383d610
acbuynak <acbuynak@gmail.com>
2021-11-06 14:23:38 -0400

    Setup Vision Package
    Setup & Move existing python scripts into relevant package.

---
+++ atturtle_vision/scripts/atturtle_vision.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from atturtle_vision.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ atturtle_vision/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'atturtle_vision'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='acbuynak',
+    maintainer_email='acbuynak@gmail.com',
+    description='Vison Support for Camera',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'atturtle_vision = atturtle_vision.atturtle_vision:main'
+        ],
+    },
+)
