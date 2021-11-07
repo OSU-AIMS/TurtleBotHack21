b3da0a752b196ce186d3f13a43f32cd2d24cf1a6
acbuynak <acbuynak@gmail.com>
2021-11-06 21:10:37 -0400

    Add output image publisher to a new topic
    * new output topic name:  /processed_vision/target_image

--- atturtle_vision/scripts/atturtle_vision.py
+++ atturtle_vision/scripts/atturtle_vision.py
@@ -24,72 +24,84 @@ import cv2
 
 
 
-def runner(data):
-
-    """
-    Callback function for image subscriber, every frame gets scanned for board and publishes to board_center topic
-    (for robot movement) and board tile centers (for game state updates)
-    :param camera_data: Camera data input from subscriber
-    """
-    
-
-    try:
-        pub = rospy.Publisher("/color/image_raw", Image, queue_size=10)
-        bridge = CvBridge()
-        cv_image = bridge.imgmsg_to_cv2(data, desired_encoding = 'passthrough')
-        #cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        #LBDet = cv2.CascadeClassifier('haarcascade_lowerbody.xml')
-
-        hog = cv2.HOGDescriptor()
-        hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-        while(True):
-
-            #color = imutils.resize(cv_image,
-            #                       width=min(400, color.shape[1]))
-
-            #color = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
-            #LB = LBDet.detectMultiScale(cv_image,1.1,4)
-            #UB = UPDet.detectMultiScale(color,1.05,5)
-            #FB = FBDet.detectMultiScale(color,1.1,4)
-            (regions, _) = hog.detectMultiScale(cv_image,
-                                                winStride=(4, 4),
-                                                padding=(4, 4),
-                                                scale=1.05)
-
-            # Drawing the regions in the Image
-            for (x, y, w, h) in regions:
-                cv2.rectangle(cv_image, (x, y),
-                              (x + w, y + h),
-                              (255, 0, 0), 2)
-                print(x)
-                print(y)
 
 
-            # Drawing the regions in the Image
-            #for (x, y, w, h) in LB:
-            #    cv2.rectangle(cv_image, (x, y),
-            #                  (x + w, y + h),
-            #                  (0, 0, 0), 2)
 
-            # Showing the output Image
-            #cv2.imshow("Image", color)
-            out_image = bridge.cv2_to_imgmsg(cv_image, "bgr8")
-            pub.publish(out_image)
+class EnvisionTarget():
+
+
+    def __init__(self, pub_targetImage):
+        """Initialize
+            Setup Publishers as class variables.
+        """
+
+        # Setup connection to Publisher for Output Images
+        self.pub_targetImage = pub_targetImage  
+
+
+        # Tools
+        self.bridge = CvBridge()
 
 
 
 
+    def runner(self, data):
+
+        """
+        Callback function for image subscriber, every frame gets scanned for board and publishes to board_center topic
+        (for robot movement) and board tile centers (for game state updates)
+        :param camera_data: Camera data input from subscriber
+        """
+        
+
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding = 'passthrough')
+            #cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            #LBDet = cv2.CascadeClassifier('haarcascade_lowerbody.xml')
+
+            hog = cv2.HOGDescriptor()
+            hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+            while(True):
+
+                #color = imutils.resize(cv_image,
+                #                       width=min(400, color.shape[1]))
+
+                #color = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+                #LB = LBDet.detectMultiScale(cv_image,1.1,4)
+                #UB = UPDet.detectMultiScale(color,1.05,5)
+                #FB = FBDet.detectMultiScale(color,1.1,4)
+                (regions, _) = hog.detectMultiScale(cv_image,
+                                                    winStride=(4, 4),
+                                                    padding=(4, 4),
+                                                    scale=1.05)
+
+                # Drawing the regions in the Image
+                for (x, y, w, h) in regions:
+                    cv2.rectangle(cv_image, (x, y),
+                                  (x + w, y + h),
+                                  (255, 0, 0), 2)
+                    print(x)
+                    print(y)
+
+
+                # Drawing the regions in the Image
+                #for (x, y, w, h) in LB:
+                #    cv2.rectangle(cv_image, (x, y),
+                #                  (x + w, y + h),
+                #                  (0, 0, 0), 2)
+
+                # Showing the output Image
+                #cv2.imshow("Image", color)
+                out_image = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+                self.pub_targetImage.publish(out_image)
 
 
 
-
-
-
-    except rospy.ROSInterruptException:
-        exit()
-    except KeyboardInterrupt:
-        exit()
+        except rospy.ROSInterruptException:
+            exit()
+        except KeyboardInterrupt:
+            exit()
 
 
 
@@ -109,8 +121,16 @@ def main():
     rospy.loginfo(">> Vision Processor Node Successfully Created")
 
 
+    # Setup ROS Publishers
+    pub_target = rospy.Publisher("/processed_vision/target_image", Image, queue_size=15)
+
+
+    # Setup Class
+    visionClass = EnvisionTarget(pub_target)
+
+
     # Setup Listeners
-    image_sub = rospy.Subscriber("/color/image_raw", Image, runner)
+    image_sub = rospy.Subscriber("/color/image_raw", Image, visionClass.runner)
 
 
     # Auto-Run until launch file is shutdown
